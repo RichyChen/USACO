54621957b87d18d67307a9e7fbfdcd248b818494
RichyChen <cmm620@gmail.com>
2016-07-20 22:06:42 +0800

    The first two files Readme and Combination_Lock.cpp

---
+++ Combination_Lock.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: Combination_Lock.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年07月20日 星期三 22时04分29秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
